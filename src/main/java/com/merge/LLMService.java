eac6f26f4a795afb230b2cfb4c155bfc51642650
abimeetsyou@gmail.com <abimeetsyou@gmail.com>
2025-12-21 11:57:47 +0530

    Initial commit

---
+++ src/main/java/com/merge/LLMService.java
@@ -0,0 +1,9 @@
+package com.merge;
+
+public interface LLMService {
+	
+		String callLLM(String systemPrompt, String userPrompt, String modelName, String apiURL) throws Exception;
+		
+		String getAPIKey(String modelName) throws Exception;
+		
+}
